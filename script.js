1c4c63f1ace505966c594ef2f03bd844a77c50f8
Rullian Rong <naillur.gnor@gmail.com>
2022-01-14 22:30:02 +0800

    Fixed code indentions and line breaks for better readability

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//function that generates random number from 0-2 and returns a string based on the generated number
-//this works as the main engine of the computer/opponent
+//function that generates random number from 0-2 and returns a 
+//string based on the generated number this works as the
+//engine of the computer/opponent
 function computerPlay() {
     let numGenerator = Math.floor(Math.random() * 3);
 
@@ -13,16 +14,27 @@ function computerPlay() {
 }
 
 //function that determines who won the round by comparing 2 parameters:
-//the user's input and computers generated string
+//the user's input and computers input
 function playRound(playerSelection, computerSelection) {
+
     let decision;
 
     if (playerSelection === computerSelection) {
+
         decision = "It's a Tie!\n ";
-    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+
         decision = "You won this round!\n ";
-    } else if ((playerSelection === "rock" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock")) {
+
+    } else if ((playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")) {
+
         decision = "You lose this round!\n ";
+
     }
 
     console.log("You chose: " + playerSelection);
@@ -34,30 +46,42 @@ function playRound(playerSelection, computerSelection) {
 
 //main game engine
 function game() {
+
     let userScore = 0,
         computerScore = 0;
     let result;
 
     //program that will loop playRound() until there's a decided winner
     while ((userScore < 5) && (computerScore < 5)) {
+
         let playerSelection = prompt("Please choose your weapon: ").toLowerCase();
         let computerSelection = computerPlay();
         result = playRound(playerSelection, computerSelection); //each rounds data will be stored here
 
         if (result === "You won this round!\n ") {
+
             ++userScore;
+
         } else if (result === "You lose this round!\n ") {
+
             ++computerScore;
+
         } else {
+
             console.log("User: " + userScore);
             console.log("Computer: " + computerScore + "\n**********");
             continue;
+
         }
 
         console.log("User: " + userScore);
         console.log("Computer: " + computerScore + "\n**********");
+
     }
+
     return (userScore == 5 ? "CONGRATULATIONS! YOU WON THE GAME!" : "SORRY, YOU LOSE.");
+
 }
 
+console.log(game());
 console.log(game());
