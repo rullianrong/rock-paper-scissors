49a8a55b641feb93fe40462250349caf835a978a
Rullian Rong <naillur.gnor@gmail.com>
2022-01-16 16:20:35 +0800

    refactored and rewrite code of the site

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+let userScore = 0,
+    computerScore = 0;
+
+const compChoice = document.querySelector('#computer-choice');
+const results = document.querySelector('#result');
+const userTotal = document.querySelector('#user-score');
+const compTotal = document.querySelector('#comp-score');
+
 //function that generates random number from 0-2 and returns a 
 //string based on the generated number this works as the
 //engine of the computer/opponent
 function computerPlay() {
-
     let numGenerator = Math.floor(Math.random() * 3);
-
     if (numGenerator === 0) {
-
         return "rock";
-
     } else if (numGenerator === 1) {
-
         return "paper";
-
     } else {
-
         return "scissors";
     }
 }
@@ -22,13 +23,9 @@ function computerPlay() {
 //function that determines who won the round by comparing 2 parameters:
 //the user's input and computers input
 function playRound(playerSelection, computerSelection) {
-
     let decision;
-
     if (playerSelection === computerSelection) {
-
         decision = "It's a Tie!\n ";
-
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
@@ -40,53 +37,36 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === "scissors" && computerSelection === "rock")) {
 
         decision = "You lose this round!\n ";
-
     }
-
-    console.log("You chose: " + playerSelection);
-    console.log("Computer chose: " + computerSelection + "\n ");
-    console.log(decision);
-
-    return decision;
+    result(decision, computerSelection);
 }
 
-//main game engine
-function game() {
+function result(decision, computerSelection) {
+    if ((userScore < 5) && (computerScore < 5)) {
+        compChoice.innerHTML = "Computer chose: " + computerSelection;
+        results.innerHTML = decision;
 
-    let userScore = 0,
-        computerScore = 0;
-    let result;
+        if (decision == "You won this round!\n ") userScore++;
+        else if (decision == "You lose this round!\n ") computerScore++;
 
-    //program that will loop playRound() until there's a decided winner
-    while ((userScore < 5) && (computerScore < 5)) {
-
-        let playerSelection = prompt("Please choose your weapon: ").toLowerCase();
-        let computerSelection = computerPlay();
-        result = playRound(playerSelection, computerSelection); //each rounds data will be stored here
-
-        if (result === "You won this round!\n ") {
-
-            ++userScore;
-
-        } else if (result === "You lose this round!\n ") {
-
-            ++computerScore;
-
+        userTotal.innerHTML = userScore;
+        compTotal.innerHTML = computerScore;
+    }
+    if ((userScore == 5) || (computerScore == 5)) {
+        if (userScore > computerScore) {
+            compChoice.innerHTML = "Congratulations! You Win!";
+            results.innerHTML = '';
         } else {
-
-            console.log("User: " + userScore);
-            console.log("Computer: " + computerScore + "\n**********");
-            continue;
-
+            compChoice.innerHTML = "Sorry, you lost.";
+            results.innerHTML = '';
         }
-
-        console.log("User: " + userScore);
-        console.log("Computer: " + computerScore + "\n**********");
-
     }
-
-    return (userScore == 5 ? "CONGRATULATIONS! YOU WON THE GAME!" : "SORRY, YOU LOSE.");
-
 }
 
-console.log(game());
+const btn = document.querySelectorAll('button');
+btn.forEach(button => {
+    button.addEventListener('click', thisButton => {
+        let playerSelection = thisButton.target.id;
+        playRound(playerSelection, computerPlay());
+    });
+})
